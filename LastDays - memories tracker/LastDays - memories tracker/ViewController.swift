931b394dc8f873818affdba9578fc7b7d3afbcef
FeeJey <148060961+FeeJey@users.noreply.github.com>
2024-03-25 11:30:19 +0100

    Add files via upload

---
+++ LastDays - memories tracker/LastDays - memories tracker/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  LastDays - memories tracker
+//
+//  Created by Firdavs Juraev on 25/03/24.
+
+//Introducing [App Name], a new app that helps you keep track of your thoughts, ideas, and plans for the future. After learning about app development, we came up with this idea to create something that can remind you of your past and inspire you for the future.
+//
+//With Last Days - memories tracker, you can easily jot down anything that comes to mind—whether it's a goal you want to achieve, a memory you want to keep, or just a random thought. The app keeps everything organized so you can find it later.
+//
+//We've designed 'Last Days' to be simple and easy to use. You can quickly navigate through your notes, create different categories, and even set reminders to review your past entries.
+//
+//But 'Last Days' is more than just a note-taking app—it's like having a personal coach for your thoughts. It helps you stay mindful, encourages personal growth, and celebrates your progress along the way.
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
